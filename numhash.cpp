5143a03c5eef011db87806dcff56f5243c569ee4
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-07-15 17:50:31 +0530

    Number hashing using maps STL

---
+++ numhash.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    //pre-computing
+    map<int,int> mp;
+    for(int i = 0; i<n; i++){
+        mp[arr[i]]++;
+    }
+
+    int q;
+    cin >> q;
+    while(--q){
+        int number;
+        cin >> number;
+        //fetch
+        cout << mp[number] <<endl; //prints the value corresponding to the number in mp
+    }
+    return 0;
+};
