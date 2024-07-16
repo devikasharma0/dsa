2e6c88f31070e20d5ba5894b49da18a381c2ebff
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-07-16 21:32:00 +0530

    selection sort

---
+++ selectionsort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+    for(int i = 0; i<n-1; i++){
+        int min = i;
+        for(int j = i; j<= n-1; j++){
+            if(arr[j]< arr[min]){
+                 min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    selection_sort(arr,n);
+    for(int i= 0; i<n;i++){
+        cout<< arr[i]<<" ";
+    }
+    return 0;
+};
