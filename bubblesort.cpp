030a2359189c015a53db0d97374ed8f5150dfa70
devikasharma0 <89690020+devikasharma0@users.noreply.github.com>
2024-07-16 22:39:51 +0530

    update bubble sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 void bubble_sort(int arr[], int n){
     for(int i = n-1; i>0; i--){
+        int didswap = 0; 
         for(int j = 0; j<=i-1; j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp; 
+                didswap = 1;
             }
+        }
+        if(didswap == 0){
+            break;
         }
     }
 }
